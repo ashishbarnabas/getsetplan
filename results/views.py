edba02e1d8fcae38f5c53fb50d4927e73677ef96
ashishbarnabas <ashish.barnabas@gmail.com>
2018-02-17 21:40:14 +1000

    results and dashboard apps and routing added

--- results/views.py
+++ results/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import HttpResponse, request
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Results Page')
