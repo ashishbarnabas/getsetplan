2b773b9d52b7866eb8e2b5e2e6cf23553be83971
jagatpaljpg <jagatpaljpg@gmail.com>
2018-02-21 17:16:02 +0530

    Updated the event calendar

--- static_raw/js/calendar.js
+++ static_raw/js/calendar.js
@@ -1,60 +1,50 @@
 $(document).ready(function() {
 
-    $('#calendar').fullCalendar({
-      header: {
-        left: '',
-        center: 'Title',
-        right: ''
-      },
-      views: {
-        agendaFourDay: {
-            type: 'agenda',
-            duration: { days: 4 },
-            buttonText: '4 day'
-        },
-        month: { 
-            titleFormat: 'MMM'
-        }
-    },
-        defaultView: 'agendaFourDay',
-        /* minTime: "00:00:00"  show as 12 AM and 24:00:00 as 11PM */
-        minTime: "00:00:00",
-        maxTime: "24:00:00",
-        slotDuration: "00:30:00",
-        defaultDate: '2018-01-01',
-        allDay: false,
-        editable: true,      
+    $('#calendar').fullCalendar(
+      {
+        defaultView: 'agendaTwoDay',
+        defaultDate: '2018-02-07',
+        resourceLabelText: 'Rooms',
+        editable: true,
+        selectable: true,
+        eventLimit: true,
+        groupByResource: true,
 
-        events: [
-        {
-          title: 'Business Lunch',
-          start: '2018-01-01T01:00:00',
-          color: '#ff9f89',
-          description: 'This is a cool event'
-        },
-        {
-          title: 'Meeting',
-          start: '2018-01-06T11:00:00',
-          color: '#257e4a',
-          description: 'This is a cool event'
-        },
-        {
-          title: 'Conference',
-          start: '2018-01-02T11:00:00',
-          end: '2018-01-02T03:00:00',
-          description: 'This is a cool event'
-        },
-        {
-          title: 'Party',
-          start: '2018-01-01T20:00:00',
-          description: 'This is a cool event'
+        header: {
+            left: 'prev,next',
+            center: 'Title',
+            right: ''
+          },
+
+        views: {
+          agendaTwoDay: {
+              type: 'agenda',
+              duration: { days: 4 }
+            },
+          },
+
+      events: [
+          {start: '2018-02-07', end: '2018-02-07', className: 'destination', title: 'New Delhi' },
+          {start: '2018-02-08', end: '2018-02-08', className: 'destination', title: 'Agra' },
+          {start: '2018-02-09', end: '2018-02-09', className: 'destination', title: 'jaipur' },
+          { id: '1', start: '2018-02-08T09:00:00', end: '2018-02-08T14:00:00', className: 'visitEvent', title: 'event 2', description: 'FullCalendar with Event Modal Dialog Example. This demo shows how to have a jQuery UI modal dialog on event-click when using FullCalendar' },
+          { id: '2', start: '2018-02-09T12:00:00', end: '2018-02-09T06:00:00', className: 'visitEvent', title: 'event 3', description: 'FullCalendar with Event Modal Dialog Example. This demo shows how to have a jQuery UI modal dialog on event-click when using FullCalendar' },
+          { id: '3', start: '2018-02-10T07:30:00', end: '2018-02-10T09:30:00', className: 'visitEvent', title: 'event 4', description: 'FullCalendar with Event Modal Dialog Example. This demo shows how to have a jQuery UI modal dialog on event-click when using FullCalendar' },
+          { id: '4', start: '2018-02-11T10:00:00', end: '2018-02-11T15:00:00', className: 'visitEvent', title: 'event 5', description: 'FullCalendar with Event Modal Dialog Example. This demo shows how to have a jQuery UI modal dialog on event-click when using FullCalendar' }
+        ],
+
+        eventClick: function (event, jsEvent, view) {
+
+            $("#title").text(event.title);
+            $("#description").text(event.description);
+
+            $('#dialog').css('display', 'block');
         }
-      ],
-    eventClick: function(event, element) {
-        element.qtip({
-            content: event.description
-        });
-    }
+
     });
- 
+
+    $('.close').click(function(){
+      $('#dialog').css('display', 'none');
+    });
+
   });
