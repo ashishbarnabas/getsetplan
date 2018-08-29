c08a7334a600c3fbe6d08ba40b215bdfabc16c10
Vineet Upadhyay <vinnieupadhyay@gmail.com>
2018-08-29 09:34:12 +0530

    added google place api

--- api_proj/api/urls.py
+++ api_proj/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
+    url(r'^maps/', views.MapApiView.as_view()),
 ]
--- api_proj/api/views.py
+++ api_proj/api/views.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import serializers
+import requests
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -26,3 +26,14 @@ class HelloApiView(APIView):
 
             return Response({'message': message})
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+class MapApiView(APIView):
+
+    def get(self, request):
+
+        api_key = ''
+        location = 'Paris'
+        req = requests.get('https://maps.googleapis.com/maps/api/place/autocomplete/json?input='+ location +'&types=geocode&key=AIzaSyAlI_1ZjTv7oU055ip-mdL4PEXzA9Lk0xY')
+        return Response(req)
+    
